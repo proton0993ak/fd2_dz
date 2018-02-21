4c9fe4bda485f0ec3f14b2ba175b8fdf71117193
Дмитрий <proton0993ak@gmail.com>
2018-02-21 23:12:44 +0300

    TREESUM

--- hw/TREESUM.js
+++ hw/TREESUM.js
@@ -1,22 +1,65 @@
 "use strict"
-var a = [ 5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ],  1, 8 ];
-var f = function(a){
-  
-     var s = 0;
 
-     for(var i=0; i<a.length; i++){
-                var item = a[i];
-                if(typeof(item) == 'number'){
-                          s+= item;
-                         
-                } else{
-                      s+= f(item);        
-                }
+var massiv = [ 5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ],  1, 8 ];
 
-               
-     }
-  return s;
-  s
+var g = function(massiv){
+    var summaMassiva = 0;
+    for(var i=0; i<massiv.length; i++){
+        var rez = massiv[i];
+        if(typeof(rez) == 'number'){
+            summaMassiva += rez;
+        }else{
+            summaMassiva += g(rez);
+        }
+    }
+
+    return summaMassiva;
 }
 
-console.log(f(a));
+console.log("Сумма всех элементов данного массива = " + g(massiv));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var a = [ 5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ],  1, 8 ];
+// var f = function(a){
+  
+//      var s = 0;
+
+//      for(var i=0; i<a.length; i++){
+//                 var item = a[i];
+//                 if(typeof(item) == 'number'){
+//                           s+= item;
+                         
+//                 } else{
+//                       s+= f(item);        
+//                 }
+
+               
+//      }
+//   return s;
+//   s
+// }
+
+// console.log(f(a));
