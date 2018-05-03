61c799839aed52326ff1239538b8cdd683714c90
Дмитрий <proton0993ak@gmail.com>
2018-05-03 17:43:58 +0300

    18

--- DRINKS_HASH/DRINKS_HASH.js
+++ DRINKS_HASH/DRINKS_HASH.js
@@ -1,32 +1,146 @@
-"use strict";
+"use strict"
 
-function HashStorage(){
+// function ClassF(){
 
-    // Спрашивает название напитка
+//     this.a = 2;
 
-    var nameNapitok = prompt("Введите название напитка");
-
-    // Спрашивает алкогольный он или нет
-
-    var alkOrnoalc = prompt("Напиток алкогольный  или без алкогольный?");
-
-    // Спрашивает  рецепт его приготовления
-
-    var recept = prompt("Какой рецепт у напитка?");
-
-    // сохраняет информацию о напитке в хранилище.
+//     this.sado = function(){
+//             console.log("obj " + this);
+//             console.log("thisa " + this.a);
+            
+//     } 
 
 
-    var t = this;
+   
+// }
 
-    t.value = null;
-    t.key = null;
-
-    t.addValue = function(key, value){ t.value = value;  t.key = key; return t; };
-    t.show = function(){console.log(t.value + " " + t.key); return t;}
-}
+// function Ciclope(){
+//     this.a = 1;
+//     this.sado = bFunc;
+// }
 
 
-var drinkStorage = new HashStorage();
+function ff(a,b) { 
+    console.log(a+" "+b); 
+    console.log(this); 
+    console.log(this.k); 
+  }
+  
+  var hh={k:77};
+  var ff2=ff.bind(hh);
+  ff2(5,6);
+
+// var b = new ClassF(); 
+// var bFunc =  b.sado;
+
+// bFunc();
+
+
+// var c = new Ciclope();
+// c.sado();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "use strict";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function HashStorage(){
+
+//     var self = this;
+//     self.nameNapitok = null;
+//     self.alkOrnoalc = null;
+//     self.recept = null;
+
+//     self.addValue
+
+
+    
+
+
+
+
+//     self.setNapitok = function(){
+        
+//         self.nameNapitok = prompt("Введите название напитка");
+//         self.alkOrnoalc =  confirm("Напиток алкогольный  или без алкогольный? (  OK - да  / ОТМЕНА - нет  )");
+//         self.recept = prompt("Какой рецепт у напитка?");
+//         if(self.alkOrnoalc == true){
+//             self.alkOrnoalc = "да";
+//         }else{ self.alkOrnoalc = "нет"}
+        
+
+//         return self;
+//     }
+
+//     self.show = function(){
+        
+//         console.log(
+//             "напиток "  + self.nameNapitok + "\n" +
+//             "алкогольный: " +  self.alkOrnoalc + "\n" +
+//             "рецепт приготовления: " + "\n" +
+//             self.recept
+
+//         );
+
+//         return self;
+//     }
+
+   
+// }
+
+
+// (new HashStorage()).setNapitok().show();
+// (new HashStorage()).setNapitok().show();
+
+
+
 
 
